c1a1b06b29ae82bd1a26d1c37672a98b710c3cb7
Maximiliano <maximilianogalindo7@gmail.com>
2023-03-04 16:04:18 -0600

    ...

--- PythonDS/Chapter1/harvest.py
+++ PythonDS/Chapter1/harvest.py
@@ -1,5 +1,6 @@
-#Problem 1: In a one-dimesional array, the total number of tons of cereals harvested over each 
-#month of the last year has been stored. Write a program that gets and prints the following information.
+#Problem 1: In a one-dimesional array, the total number of tons of cereals harvested over  
+#each month of the last year has been stored. Write a program that gets and prints 
+#the following information.
 #a) The annual average of harvested tons.
 #b) How many months did they have a harvest above the annual average?
 #c) How many months did they have a lower harvest than the annual average?"
@@ -27,7 +28,7 @@ def run():
         if mean>i:
             l = l+1
 
-    print(TonsM)
+    
     print("a): ", mean)
     print("b): ", a)
     print("c): ", l)
